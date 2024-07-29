0b459db5891031bc732169a38bbae5ed6ff6bb8b
mahmoudaldaly <mahmo@DESKTOP-PVTMUIB>
2024-07-29 23:50:59 +0300

    Swap Overloading

---
+++ Swap/Helper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swap
+{
+	internal class Helper
+	{
+        
+		public static void Swap(ref int x,ref int y)
+		{
+			int Temp = x;
+			x = y;
+			y = Temp;
+
+		}
+
+		public static void Swap(ref double x, ref double y)
+		{
+			double Temp = x;
+			x = y;
+			y = Temp;
+
+		}
+
+		public static void Swap(ref Point x, ref Point y)
+		{
+            Console.WriteLine("___________Swap________________");
+            Point Temp = x;
+			x = y;
+			y = Temp;
+
+		}
+	}
+}
--- Swap/Program.cs
+++ Swap/Program.cs
@@ -4,7 +4,45 @@
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			int a = 10;
+			int b = 7;
+
+            Console.WriteLine($"A = {a}");
+            Console.WriteLine($"B = {b}");
+
+            Console.WriteLine("___________________________");
+
+            Helper.Swap(ref a,ref b );
+			Console.WriteLine($"A = {a}");
+			Console.WriteLine($"B = {b}");
+
+			Console.WriteLine("___________________________");
+
+			double d = 7.7;
+			double c = 10.10;
+			Console.WriteLine($"D = {d}");
+			Console.WriteLine($"C = {c}");
+
+			Console.WriteLine("___________________________");
+
+			Helper.Swap(ref d, ref c);
+			Console.WriteLine($"D = {d}");
+			Console.WriteLine($"C = {c}");
+
+            Console.WriteLine("____________________________");
+
+			Point p1 = new Point(3,5);
+			Point p2 = new Point(7,9);
+
+			Console.WriteLine($"p1 = {p1}");
+			Console.WriteLine($"p2 = {p2}");
+
+			Console.WriteLine("____________________________");
+
+			Helper.Swap(ref p1,ref p2);
+
+			Console.WriteLine($"p1 = {p1}");
+			Console.WriteLine($"p2 = {p2}");
 		}
 	}
 }
